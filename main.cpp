cc1adf7d700fd035eb0221e4632e811d26e0fd28
lake <102535481@qq.com>
2024-01-30 18:41:23 +0800

    m

--- main.cpp
+++ main.cpp
@@ -5,65 +5,141 @@
 #include <opencv2/imgproc.hpp>
 
 using namespace std;
+using namespace cv;
+
+int imgMatchTemplate();
 
 int main() {
-    string img_path = "/Users/lake/CLionProjects/opencv-test/iphone.png";
+    float result = pow(2-0.80, 4);
+    cout << result << endl;
+    imgMatchTemplate();
+    return 0;
+}
+
+vector<Mat> buildTemplates(Mat templ) {
+    vector<Mat> templates = vector<Mat>();
+    templates.push_back(templ);
+    float resizeRetain = 0.8;
+    for (int i = 0; i < 4; i++) {
+        Mat resizeTempl;
+        //放大
+        float resizeLargeRetain = pow(2 - resizeRetain, i + 1);
+        resize(templ, resizeTempl, Size(0,0), resizeLargeRetain, resizeLargeRetain);
+        templates.push_back(resizeTempl);
+
+        //缩小
+        float resizeSamllReduce = pow(resizeRetain, i + 1);
+        resize(templ, resizeTempl, Size(0,0), resizeSamllReduce, resizeSamllReduce);
+        templates.push_back(resizeTempl);
+    }
+    return templates;
+}
+
+int imgMatchTemplate() {
+    string img_path = "/Users/lake/dounine/github/apple/opencv-learn/iphone.png";
     cv::Mat image = cv::imread(img_path);
-    cv::Mat find_image = cv::imread("/Users/lake/CLionProjects/opencv-test/find.png", cv::IMREAD_GRAYSCALE);
+    cv::Mat find_image = cv::imread("/Users/lake/dounine/github/apple/opencv-learn/find.png", cv::IMREAD_GRAYSCALE);
     if (image.empty()) {
         std::cout << "Could not read the image: " << img_path << std::endl;
         return 1;
     }
-    cv::Mat gray_image;
-    cv::cvtColor(image, gray_image, cv::COLOR_RGB2GRAY);
+    Mat gray_image;
+    cvtColor(image, gray_image, COLOR_RGB2GRAY);
 
-    //构建结果矩阵
-    cv::Mat result(gray_image.rows - find_image.rows + 1, gray_image.cols - find_image.cols + 1, CV_32FC1);
-    //模板匹配
-    cv::matchTemplate(gray_image, find_image, result, cv::TM_CCOEFF);
 
-    double dMaxVal;//分数最大值
-    cv::Point ptMaxLoc;//最大值坐标
-    //寻找结果矩阵中的最大值
-    cv::minMaxLoc(result, nullptr, &dMaxVal, nullptr, &ptMaxLoc);
+    //计算匹配耗时
+    long start = clock();
+    vector<Mat> templates = buildTemplates(find_image);
+    bool isFind = false;
+    for (int i = 0; i < templates.size(); i++) {
+        Mat tmpl = templates[i];
 
-    std::cout << "dMaxVal: " << dMaxVal << std::endl;
+        int result_rows = gray_image.cols - tmpl.cols + 1;
+        int result_cols = gray_image.rows - tmpl.rows + 1;
 
-    //绘制矩形
-    cv::Point pt1(ptMaxLoc.x, ptMaxLoc.y);
-    cv::Point pt2(ptMaxLoc.x + find_image.cols, ptMaxLoc.y);
-    cv::Point pt3(ptMaxLoc.x, ptMaxLoc.y + find_image.rows);
-    cv::Point pt4(ptMaxLoc.x + find_image.cols, ptMaxLoc.y + find_image.rows);
+        //构建结果矩阵
+        Mat result(result_rows, result_cols, CV_32FC1);
+        //模板匹配
+        matchTemplate(gray_image, tmpl, result, TM_CCORR_NORMED);
 
-    //画线
-    cv::line(image, pt1, pt2, cv::Scalar(0, 255, 0), 11, 11, 1);
-    cv::line(image, pt2, pt4, cv::Scalar(0, 255, 0), 11, 11, 1);
-    cv::line(image, pt4, pt3, cv::Scalar(0, 255, 0), 11, 11, 1);
-    cv::line(image, pt3, pt1, cv::Scalar(0, 255, 0), 11, 11, 1);
+        double minValue, maxValue;//分数最大值
+        Point minLocation, maxLocation;//最大值坐标
+        //寻找结果矩阵中的最大值
+        minMaxLoc(result, &minValue, &maxValue, &minLocation, &maxLocation, Mat());
 
-    cv::imshow("hello", image);
-    cv::waitKey(0);
+        std::cout << "dMaxVal: " << maxValue << std::endl;
+
+        if (maxValue >= 0.9) {
+            isFind = true;
+            rectangle(image, Rect(
+                    maxLocation.x,
+                    maxLocation.y,
+                    tmpl.cols,
+                    tmpl.rows
+            ), Scalar(0, 255, 0), 10);
+            imshow("hello", image);
+            // x:963 y:1815 width:245 height:233 //正确的
+            cout << "找到了 x:" << maxLocation.x << " y:" << maxLocation.y << " width:" << tmpl.cols << " height:"
+                 << tmpl.rows << endl;
+            break;
+        }
+    }
+    long end = clock();
+    std::cout << "耗时: " << (end - start) / 1000 << "ms" << std::endl;
+    if (!isFind) {
+        std::cout << "未找到" << std::endl;
+    }
+//    imshow("hello", image);
+    waitKey(0);
 //    cv::imshow("hello", dest);
-//    cv::waitKey(0);
+//    waitKey(0);
 
-//    cv::VideoCapture cap(0);
+//    VideoCapture cap(0);
 //    if (!cap.isOpened()) {
 //        std::cout << "Could not open the camera" << std::endl;
 //        return 1;
 //    }
-//    cv::Mat frame;
+//    Mat frame;
 //    while(true){
 //        cap >> frame;
 //        if (frame.empty()) {
 //            std::cout << "Could not read the frame" << std::endl;
 //            break;
 //        }
-//        cv::imshow("hello", frame);
-//        if (cv::waitKey(10) == 27) {
+//        imshow("hello", frame);
+//        if (waitKey(10) == 27) {
 //            std::cout << "Esc key is pressed by user. Stopping the video" << std::endl;
 //            break;
 //        }
 //    }
     std::cout << "Hello, World!" << std::endl;
-    return 0;
 }
+
+//void matchMethod(int,void*){
+//    Mat img_display;
+//    img.copyTo(img_display);
+//    int result_cols = img.cols - templ.cols + 1;
+//    int result_rows = img.rows - templ.rows + 1;
+//    //构建结果矩阵
+//    Mat result(img.rows - templ.rows + 1, img.cols - templ.cols + 1, CV_32FC1);
+//    result.create(result_rows, result_cols, CV_32FC1);
+//    matchTemplate(img, templ, result, match_method);
+//    normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());//将结果归一化防止溢出，显示
+//    double minVal; double maxVal; Point minLoc; Point maxLoc;
+//    Point matchLoc;
+//    minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());//不同方法对应最小值与最大值不同
+//    if (match_method == TM_SQDIFF || match_method == TM_SQDIFF_NORMED)
+//    {
+//        matchLoc = minLoc;
+//    }
+//    else
+//    {
+//        matchLoc = maxLoc;
+//    }
+//    rectangle(img_display, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), Scalar::all(0), 2, 8, 0);//绘制矩形
+//    // rectangle(result, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), Scalar::all(0), 2, 8, 0);
+//    imshow(image_window, img_display);
+//    imshow(result_window, result);
+//    imshow("template", templ);
+//    return;
+//}
